3185dabc5f07422d6dc335905cc50290ce44071d
Dávid Balatoni <balcsida@gmail.com>
2018-01-11 10:48:27 +0100

    Initial commit

---
+++ php/sample_code/conf/defines.php
@@ -0,0 +1,11 @@
+<?php 
+
+
+
+define("BASE_URL","https://oauth.withings.com/account/");
+define("URLWSAPI","http://wbsapi.withings.net/");
+define("CALLBACK_URL","http://www.example.com/callback.php");
+define("OAUTH_CONSUMER_KEY","YOUR_CONSUMER_KEY");
+define("OAUTH_CONSUMER_SECRET","YOUR_CONSUMER_SECRET");
+define("LOG_BASE_PATH", "/var/log/withings");
+
